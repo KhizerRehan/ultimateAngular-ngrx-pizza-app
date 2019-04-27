ed5b63be962b6bca59f4a41214e71755949b2bd1
khizerrehan <khizerrehan92@gmail.com>
2019-04-27 13:31:37 +0500

    coreEssentials: code factoring

--- src/products/store/actions/pizza-action-creators.ts
+++ src/products/store/actions/pizza-action-creators.ts
@@ -1,21 +1,21 @@
-import * as pizzaActions from './pizza-actions';
+import * as fromPizzaActionsConstants from './pizza-actions';
 import {Action} from '@ngrx/store';
 import {Pizza} from "../../models/pizza.model";
 
 // Action Creators:
 export class LoadPizzas implements Action {
-  readonly type = pizzaActions.LOAD_PIZZAS;
+  readonly type = fromPizzaActionsConstants.LOAD_PIZZAS;
 }
 
 export class LoadPizzasFail implements Action {
-  readonly type = pizzaActions.LOAD_PIZZAS;
+  readonly type = fromPizzaActionsConstants.LOAD_PIZZAS_FAIL;
 
   constructor(public payload: any) {
   }
 }
 
 export class LoadPizzasSuccess implements Action {
-  readonly type = pizzaActions.LOAD_PIZZAS;
+  readonly type = fromPizzaActionsConstants.LOAD_PIZZAS_SUCCESS;
 
   constructor(public payload: Pizza[]) {
   }
