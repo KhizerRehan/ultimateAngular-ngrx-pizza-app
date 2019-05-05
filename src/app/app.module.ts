4f2aec41f80bbee0ee91ebb5625e4cccf6c5990a
khizerrehan <khizerrehan92@gmail.com>
2019-05-05 17:02:56 +0500

    routerStateComposition/custom-router-state-serializer connecting store with router state and firing `customSerializer` function on each Router navigation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {
+   StoreRouterConnectingModule,
+   RouterStateSerializer
+} from '@ngrx/router-store';
 import { StoreModule, MetaReducer } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-import { reducers } from "./store/reducers";
+import { reducers, CustomSerializer } from "./store/reducers";
 
 // not used in production
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
@@ -40,8 +44,10 @@ export const ROUTES: Routes = [
     RouterModule.forRoot(ROUTES),
     StoreModule.forRoot(reducers, { metaReducers }),
     EffectsModule.forRoot([]),
+    StoreRouterConnectingModule,
     environment.development ? StoreDevtoolsModule.instrument() : []
   ],
+  providers:[{ provide: RouterStateSerializer, useClass: CustomSerializer}],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
